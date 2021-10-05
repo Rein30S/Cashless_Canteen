291f86c8f617b446a79ac3835efa46bf6adce548
ASUS <ASUS@192.168.56.1>
2021-10-05 11:24:40 +0700

    Tes

---
+++ src/koneksi.java
@@ -0,0 +1,33 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class koneksi {
+    Connection conn;
+    Statement stm;
+    
+    public void config(){
+        try{
+            String dbUrl = "jdbc:mysql://localhost/cashlesscanteen";
+            String username = "root";
+            String password = "";
+            conn = DriverManager.getConnection(dbUrl, username, password);
+            stm = conn.createStatement();
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+}
