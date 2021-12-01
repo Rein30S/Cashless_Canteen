85ca879738ef4a8bda7944c5c4023674f4d72d86
ASUS <ASUS@192.168.56.1>
2021-12-01 22:27:22 +0700

    Progress Toko: Setting akun

--- src/Toko/Menu_Toko.java
+++ src/Toko/Menu_Toko.java
@@ -5,6 +5,11 @@
  */
 package Toko;
 
+import Login.LoginPage;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  *
  * @author ASUS
@@ -14,8 +19,18 @@ public class Menu_Toko extends javax.swing.JFrame {
     /**
      * Creates new form Menu_Toko
      */
+    
+    Connection conn;
+    Statement stm;
+    ResultSet rs;
+    String username;
     public Menu_Toko() {
         initComponents();
+    }
+    
+    public void setData(String username){
+        this.username = username;
+        labelUsername.setText(username);
     }
 
     /**
@@ -27,6 +42,10 @@ public class Menu_Toko extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel3 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        labelUsername = new javax.swing.JLabel();
         Btn_RiwayatPenarikan = new javax.swing.JLabel();
         Btn_TarikSaldo = new javax.swing.JLabel();
         Btn_RiwayatPenjualan = new javax.swing.JLabel();
@@ -36,9 +55,44 @@ public class Menu_Toko extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel3.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jLabel3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel3MouseClicked(evt);
+            }
+        });
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(780, 140, 110, 40));
+
+        jLabel2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jLabel2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel2MouseClicked(evt);
+            }
+        });
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(850, 10, 40, 40));
+
+        jLabel1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jLabel1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel1MouseClicked(evt);
+            }
+        });
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 550, 150, 40));
+
+        labelUsername.setFont(new java.awt.Font("Tahoma", 2, 18)); // NOI18N
+        labelUsername.setText("jLabel1");
+        getContentPane().add(labelUsername, new org.netbeans.lib.awtextra.AbsoluteConstraints(750, 100, -1, -1));
         getContentPane().add(Btn_RiwayatPenarikan, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 450, 240, 60));
         getContentPane().add(Btn_TarikSaldo, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 380, 240, 60));
         getContentPane().add(Btn_RiwayatPenjualan, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 400, 240, 110));
+
+        Btn_UpdateMenu.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        Btn_UpdateMenu.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                Btn_UpdateMenuMouseClicked(evt);
+            }
+        });
         getContentPane().add(Btn_UpdateMenu, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 250, 240, 260));
 
         BG.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/Toko/Menu Page.png"))); // NOI18N
@@ -47,6 +101,31 @@ public class Menu_Toko extends javax.swing.JFrame {
         setSize(new java.awt.Dimension(900, 600));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jLabel1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel1MouseClicked
+        // TODO add your handling code here:
+        LoginPage lp = new LoginPage();
+        lp.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_jLabel1MouseClicked
+
+    private void jLabel2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel2MouseClicked
+        // TODO add your handling code here:
+        System.exit(0);
+    }//GEN-LAST:event_jLabel2MouseClicked
+
+    private void jLabel3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel3MouseClicked
+        // TODO add your handling code here:
+        Pengaturan_Akun pa = new Pengaturan_Akun();
+        pa.setData(username);
+        pa.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_jLabel3MouseClicked
+
+    private void Btn_UpdateMenuMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Btn_UpdateMenuMouseClicked
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_Btn_UpdateMenuMouseClicked
 
     /**
      * @param args the command line arguments
@@ -89,5 +168,9 @@ public class Menu_Toko extends javax.swing.JFrame {
     private javax.swing.JLabel Btn_RiwayatPenjualan;
     private javax.swing.JLabel Btn_TarikSaldo;
     private javax.swing.JLabel Btn_UpdateMenu;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel labelUsername;
     // End of variables declaration//GEN-END:variables
 }
